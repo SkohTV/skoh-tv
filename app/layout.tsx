74d52a851e312871e2992607803410a3dd20efb1
SkohTV <contact@skoh.dev>
2025-11-26 19:29:55 -0500

    expandable box

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,14 +35,29 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           </div>
         </div>
 
-        <div className="absolute right-0 top-0 w-16 h-full">
+        <div tabIndex={0} className="
+          absolute right-0 top-0
+          duration-200 overflow-hidden
+          h-full w-7
+          hover:w-16 focus:w-16
+          group
+        ">
           <div className="flex h-3/4 items-center">
-            <div className="bg-white/30 backdrop-blur-sm py-1 rounded-l-lg h-fit">
-              <ChannelPic src={LogoSkoh} size={48} bsize={2} alt='Skoh&amps;s logo' url='/skoh' />
-              <ChannelPic src={LogoNotSkoh} size={48} bsize={2} alt='NotSkoh&amps;s logo' url='' />
-              <div className="h-px w-4/5 rounded-full m-2 bg-zinc-700"></div>
-              <ChannelPic src={LogoKrystal} size={48} bsize={2} alt='Krystal&amps;s logo' url='/krystal' />
-              <ChannelPic src={LogoQuantum} size={48} bsize={2} alt='Quantum&amps;s logo' url='/quantum' />
+            <div className="flex items-center bg-white/30 border-zinc-800/70 border-2 backdrop-blur-sm py-1 rounded-l-lg h-fit">
+              <div className="
+                min-w-5 min-h-5 m-1
+                group-hover:min-w-0 group-hover:min-h-0 group-hover:m-0
+                group-focus:min-w-0 group-focus:min-h-0 group-focus:m-0
+              ">
+                <LeftArrow />
+              </div>
+              <div>
+                <ChannelPic src={LogoSkoh} size={48} bsize={2} alt='Skoh&amps;s logo' url='/skoh' />
+                <ChannelPic src={LogoNotSkoh} size={48} bsize={2} alt='NotSkoh&amps;s logo' url='' />
+                <div className="h-px group-hover:w-4/5 group-focus:w-4/5 rounded-full m-2 bg-zinc-700"></div>
+                <ChannelPic src={LogoKrystal} size={48} bsize={2} alt='Krystal&amps;s logo' url='/krystal' />
+                <ChannelPic src={LogoQuantum} size={48} bsize={2} alt='Quantum&amps;s logo' url='/quantum' />
+              </div>
             </div>
           </div>
         </div>
@@ -51,6 +66,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     </html>
   );
 }
+
+
+
+const LeftArrow = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" imageRendering="optimizeQuality" shapeRendering="geometricPrecision" textRendering="geometricPrecision" viewBox="0 0 512 463.95">
+    <path
+      fillRule="nonzero"
+      d="M512 332.66H268.5v92.31c-.68 15.47-5.77 26.46-15.43 32.82-25.79 17.2-52.31-5.26-69.24-22.6L14.33 261.6c-19.11-17.28-19.11-41.93 0-59.21L188.71 24.42c16.06-16.39 40.56-34.09 64.36-18.21 9.66 6.35 14.75 17.34 15.43 32.81v92.31H512v201.33z"
+    />
+  </svg>
+)
+
 
 
 
@@ -88,12 +115,14 @@ function ChannelPic({ src, size, bsize, alt, url }: { src: StaticImageData, size
   if (pathname === '/')
     pathname = '/skoh'
 
-  const border_size = (bsize === 2 ? 'border-2' : bsize === 4 ? 'border-4' : '')
+  const border_size = (bsize === 2 ? 'group-hover:border-2 group-focus:border-2' : '')
   const border_color = (pathname === url ? 'border-indigo-600' : 'border-zinc-800')
 
   return (
-    <Link href={url}>
-      <Image src={src} height={size} width={size} className={`rounded-full m-2 ${border_size} ${border_color}`} alt={alt} />
+    <Link href={url} className="w-[0px] h-[0px]">
+      <div className={`${border_size} ${border_color} rounded-full m-2`}>
+      <Image src={src} height={size} width={size} className="rounded-full" alt={alt} />
+      </div>
     </Link>
   )
 }
