b64c006fe6d2ae159edce57d742af68c97276278
SkohTV <contact@skoh.dev>
2025-11-28 14:47:02 -0500

    swap twitch and youtube for notskoh

--- app/notskoh/page.tsx
+++ app/notskoh/page.tsx
@@ -16,8 +16,8 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <div>
-      <Card logo={Youtube} url='https://www.youtube.com/@NotSkoh' name='Youtube' padded={false} colors={{ bg: 'bg-red-600/20', hover_bg: 'hover:bg-red-600/50', border: 'border-red-600' }} />
-      <Card logo={Twitch} url='https://twitch.tv/NotSkoh' name='Twitch' padded={true} colors={{ bg: 'bg-purple-800/20', hover_bg: 'hover:bg-purple-800/50', border: 'border-purple-800' }} />
+      <Card logo={Twitch} url='https://twitch.tv/NotSkoh' name='Twitch' padded={false} colors={{ bg: 'bg-purple-800/20', hover_bg: 'hover:bg-purple-800/50', border: 'border-purple-800' }} />
+      <Card logo={Youtube} url='https://www.youtube.com/@NotSkoh' name='Youtube' padded={true} colors={{ bg: 'bg-red-600/20', hover_bg: 'hover:bg-red-600/50', border: 'border-red-600' }} />
       <Card logo={X} url='https://x.com/notskoh' name='X' padded={true} colors={{ bg: 'bg-black/20', hover_bg: 'hover:bg-black/50', border: 'border-black' }} />
       <div className="h-4"></div>
     </div>
