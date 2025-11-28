c994c6c58873dfb3b34cdef65f6ba3734d1c0358
SkohTV <contact@skoh.dev>
2025-11-28 14:27:56 -0500

    added tiktok for tweast

--- app/tweast/page.tsx
+++ app/tweast/page.tsx
@@ -2,12 +2,13 @@ import { Card } from "../_components";
 
 import Twitch from "@/public/brand/twitch.png";
 import X from "@/public/brand/x.png";
+import Tiktok from "@/public/brand/tiktok.png";
 
 import { Metadata } from "next";
 
 
 export const metadata: Metadata = {
-  title: 'NotSkoh',
+  title: 'Tweast',
   description: 'Wip',
 }
 
@@ -17,6 +18,7 @@ export default function Page() {
     <div>
       <Card logo={Twitch} url='https://twitch.tv/tweast_tv' name='Twitch' padded={false} colors={{ bg: 'bg-purple-800/20', hover_bg: 'hover:bg-purple-800/50', border: 'border-purple-800' }} />
       <Card logo={X} url='https://x.com/TweastW' name='X' padded={true} colors={{ bg: 'bg-black/20', hover_bg: 'hover:bg-black/50', border: 'border-black' }} />
+      <Card logo={Tiktok} url='https://tiktok.com/@tweast0' name='TikTok' padded={true} colors={{ bg: 'bg-black/20', hover_bg: 'hover:bg-black/50', border: 'border-black' }} />
       <div className="h-4"></div>
     </div>
   );
