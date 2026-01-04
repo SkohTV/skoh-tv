db445c37f6d374d6335c25cb52e7844f046d0183
SkohTV <contact@skoh.dev>
2026-01-03 19:00:47 -0500

    add portfolio to skoh-dev

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -2,6 +2,7 @@ import { Card } from "../_components";
 
 import Github from "@/public/brand/github.png";
 import Linkedin from "@/public/brand/linkedin.png";
+import Link_ from "@/public/brand/link.png";
 import { Metadata } from "next";
 
 
@@ -15,6 +16,7 @@ export default function Page() {
     <div>
       <Card logo={Github} url='https://github.com/SkohTV' name='Github' padded={false} colors={{ bg: 'bg-black/20', hover_bg: 'hover:bg-black/50', border: 'border-black' }} />
       <Card logo={Linkedin} url='https://www.linkedin.com/in/noe-lorret-despret/' padded={true} name='LinkedIn' colors={{ bg: 'bg-blue-600/20', hover_bg: 'hover:bg-blue-600/50', border: 'border-blue-600' }} />
+      <Card logo={Link_} url='https://skoh.dev' padded={true} name='Portfolio' colors={{ bg: 'bg-slate-500/20', hover_bg: 'hover:bg-slate-500/50', border: 'border-slate-500' }} />
       <div className="h-4"></div>
     </div>
   );
