2313de0f15dd4f03f9499c4e651f37c2c148a619
SkohTV <contact@skoh.dev>
2026-08-22 01:25:35 +0200

    remove deeptrout discord

--- app/deeptrout/page.tsx
+++ app/deeptrout/page.tsx
@@ -15,7 +15,6 @@ export default function Page() {
   return (
     <div>
       <Card logo={Youtube} url='https://www.youtube.com/@DeepTrout' name='Youtube' padded={false} colors={{ bg: 'bg-red-600/20', hover_bg: 'hover:bg-red-600/50', border: 'border-red-600' }} />
-      <Card logo={Discord} url='' name='Discord' padded={true} colors={{ bg: 'bg-indigo-600/20', hover_bg: 'hover:bg-indigo-600/50', border: 'border-indigo-600' }} />
       <Card logo={Instagram} url='' name='Instagram' padded={true} colors={{ bg: 'bg-fuchsia-600/20', hover_bg: 'hover:bg-fuchsia-600/50', border: 'border-fuchsia-600' }} />
       <div className="h-4"></div>
     </div>
